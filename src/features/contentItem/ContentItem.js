821fee7d2a09bd579c17b23cf344e13302f37cea
masterdarklordoftheworld <adambostrom93@gmail.com>
2023-11-01 11:43:27 +0100

    added media queries and button element with styling for each article

--- src/features/contentItem/ContentItem.js
+++ src/features/contentItem/ContentItem.js
@@ -3,11 +3,13 @@ import styles from './ContentItem.module.css';
 
 function ContentItem({ id, title, preview, fullText, image }) {
     return (
-        <div className={styles.contentItem}>
-            <h2>{title}</h2>
-            <img src={image} alt="" className={styles.imageContainer} />
-            <p>{preview}</p>
-        </div>
+        <button key={id} className={styles.contentButton}>
+            <div className={styles.contentItem}>
+                <h2>{title}</h2>
+                <img src={image} alt="" className={styles.imageContainer} />
+                <p>{preview}</p>
+            </div>
+        </button>
     );
 }
 
